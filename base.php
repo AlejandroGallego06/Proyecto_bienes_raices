108a53953d7ad3af064d897d94637d9ab4ca54ce
AlejandroGallego06 <algallegoc@unal.edu.co>
2023-10-09 10:41:33 -0500

    Se agrega el proyecto

---
+++ base.php
@@ -0,0 +1,12 @@
+<?php
+require 'includes/funciones.php';
+incluirTemplate('header');
+?>
+
+<main class="contenedor seccion">
+    <h1>Título Pagina</h1>
+</main>
+
+<?php
+incluirTemplate('footer')
+?>
